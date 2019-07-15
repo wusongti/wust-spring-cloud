e345e872e5e96fb1433733ae16a4c11dd6d81e78
wust <wusongti@163.com>
2019-07-15 14:15:17 +0800

    优化

--- wust-springcloud-admin-server/src/main/java/com/wust/springcloud/admin/server/core/app/DemoApp.java
+++
@@ -1,9 +0,0 @@
-package com.wust.springcloud.admin.server.core.app;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequestMapping("/DemoApp")
-@RestController
-public class DemoApp {
-}
