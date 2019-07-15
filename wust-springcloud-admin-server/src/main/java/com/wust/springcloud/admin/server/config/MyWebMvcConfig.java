1ecd039ca626b2307a1baff16c00b539ecf88adf
wust <wusongti@163.com>
2019-07-15 18:01:11 +0800

    优化

---
+++ wust-springcloud-admin-server/src/main/java/com/wust/springcloud/admin/server/config/MyWebMvcConfig.java
@@ -0,0 +1,7 @@
+package com.wust.springcloud.admin.server.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+}
