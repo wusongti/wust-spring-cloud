21fbd322acbdf82fab5ea6f4e8e47b554a65afe2
wust <wusongti@163.com>
2019-08-09 10:17:38 +0800

    优化

---
+++ wust-springcloud-admin-server/src/main/java/com/wust/springcloud/admin/server/core/web/controller/SysUserOrganizationController.java
@@ -0,0 +1,13 @@
+package com.wust.springcloud.admin.server.core.web.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by WST on 2019/6/5.
+ */
+@RequestMapping("/SysUserOrganization")
+@RestController
+public class SysUserOrganizationController {
+
+}
