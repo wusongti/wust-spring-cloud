2d587ec15a7af0deae7de8a78c03585cc06bdfe8
wust <wusongti@163.com>
2019-08-13 13:52:06 +0800

    优化

--- wust-springcloud-sso-server/src/main/java/com/wust/springcloud/sso/server/core/rpc/api/AuthenticationRpc.java
+++ wust-springcloud-sso-server/src/main/java/com/wust/springcloud/sso/server/core/rpc/api/AuthenticationRpc.java
@@ -30,12 +30,14 @@ public class AuthenticationRpc {
     public boolean hasToken(@PathVariable String token) {
         String key = String.format(ApplicationEnum.WEB_LOGIN_KEY.getStringValue(),token);
         if(springRedisTools.hasKey(key)){ // 刷新缓存时间和token时间
+            System.out.println("刷新缓存=" +key);
             /**
              * 刷新redis缓存时间
              */
             springRedisTools.updateExpire(key, ApplicationEnum.X_AUTH_TOKEN_EXPIRE_TIME.getIntValue(), TimeUnit.MINUTES);
             return true;
         }
+        System.out.println("缓存失效=" +key);
         return false;
     }
 }
