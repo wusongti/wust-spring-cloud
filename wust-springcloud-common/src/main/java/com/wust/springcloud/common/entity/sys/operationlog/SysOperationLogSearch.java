f8e6bf99d1aa12afc0e37b0ea4b6c53fb6f5d877
wust <wusongti@163.com>
2019-08-15 11:03:06 +0800

    优化

--- wust-springcloud-common/src/main/java/com/wust/springcloud/common/entity/sys/operationlog/SysOperationLogSearch.java
+++ wust-springcloud-common/src/main/java/com/wust/springcloud/common/entity/sys/operationlog/SysOperationLogSearch.java
@@ -2,6 +2,8 @@ package com.wust.springcloud.common.entity.sys.operationlog;
 
 import com.wust.springcloud.common.dto.PageDto;
 
+import java.util.Date;
+
 /**
  * Created by WST on 2019/5/28.
  */
@@ -10,11 +12,31 @@ public class SysOperationLogSearch extends SysOperationLog{
 
     private PageDto pageDto;
 
+    private Date beginOperationDate;
+
+    private Date endOperationDate;
+
     public PageDto getPageDto() {
         return pageDto;
     }
 
     public void setPageDto(PageDto pageDto) {
         this.pageDto = pageDto;
+    }
+
+    public Date getBeginOperationDate() {
+        return beginOperationDate;
+    }
+
+    public void setBeginOperationDate(Date beginOperationDate) {
+        this.beginOperationDate = beginOperationDate;
+    }
+
+    public Date getEndOperationDate() {
+        return endOperationDate;
+    }
+
+    public void setEndOperationDate(Date endOperationDate) {
+        this.endOperationDate = endOperationDate;
     }
 }
